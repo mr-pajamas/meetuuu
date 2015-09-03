7d037e0a3fa05b860b95bf06bd2aad49a0d7a843
ck <chenkaiC4@gmail.com>
2015-09-03 17:27:42 +0800

    basic route and template setting

---
+++ client/templates/activity/create/create-activity.js
@@ -0,0 +1,21 @@
+/* created by ck 2015-09-02 */
+
+Template.createActivity.onCreated(function(){
+  console.log('onCreated');
+});
+
+Template.createActivity.onRendered(function(){
+  console.log('onRendered');
+});
+
+
+// helpers
+Template.createActivity.helpers({
+
+});
+
+
+// events
+Template.createActivity.events({
+
+});
