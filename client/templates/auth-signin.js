b046b2809001dca22d6b65ae305a9d9f0653e03c
Michael Tang <lctang@foxmail.com>
2015-10-06 15:07:10 +0800

    初步完成登录模块

---
+++ client/templates/auth-signin.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Michael on 2015/10/5.
+ */
+Template.authSignin.events({
+  "submit": function (event, template) {
+    event.preventDefault();
+
+    var mobile = template.$("[name=mobile]").val();
+    var password = template.$("[name=password]").val();
+
+    var options = {
+      mobile: mobile,
+      password: password
+    };
+
+    Meteor.commonLogin(options, function (error) {
+      if (error) {
+        alert(error.reason);
+      } else {
+        FlowRouter.go("index");
+      }
+    });
+  }
+});
