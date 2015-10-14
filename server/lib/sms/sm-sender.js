b34216064dbf41dda8b1197c8db3cd5ea66955d4
Michael Tang <lctang@foxmail.com>
2015-10-14 19:54:46 +0800

    完成手机验证

--- server/lib/sms/sm-sender.js
+++ server/lib/sms/sm-sender.js
@@ -1,6 +1,7 @@
 /**
  * Created by Michael on 2015/10/6.
  */
+/*
 var ERROR_MSGS = {
   101: "无此用户",
   102: "密码错",
@@ -56,3 +57,96 @@ var SM_ACCOUNT = "vipkkdz";
 var SM_PASSWORD = "Tch123456";
 
 smSender = new SmSender(SM_ACCOUNT, SM_PASSWORD);
+*/
+
+var ERR_MSGS = {
+  "-1": "系统异常",
+  "-101": "命令不被支持",
+  "-102": "RegistryTransInfo删除信息失败",
+  "-103": "RegistryInfo更新信息失败",
+  "-104": "请求超过限制",
+  "-111": "企业注册失败",
+  "-117": "发送短信失败",
+  "-118": "接收MO失败",
+  "-119": "接收Report失败",
+  "-120": "修改密码失败",
+  "-122": "号码注销激活失败",
+  "-123": "查询单价失败",
+  "-124": "查询余额失败",
+  "-125": "设置MO转发失败",
+  "-126": "路由信息失败",
+  "-127": "计费失败0余额",
+  "-128": "计费失败余额不足",
+  "-110": "号码注册激活失败",
+  "-1100": "序列号错误,序列号不存在内存中,或尝试攻击的用户",
+  "-1102": "序列号密码错误",
+  "-1103": "序列号Key错误",
+  "-1104": "路由失败，请联系系统管理员",
+  "-1105": "注册号状态异常, 未用 1",
+  "-1107": "注册号状态异常, 停用 3",
+  "-1108": "注册号状态异常, 停止 5",
+  "-113": "充值失败",
+  "-1131": "充值卡无效",
+  "-1132": "充值密码无效",
+  "-1133": "充值卡绑定异常",
+  "-1134": "充值状态无效",
+  "-1135": "充值金额无效",
+  "-190": "数据操作失败",
+  "-1901": "数据库插入操作失败",
+  "-1902": "数据库更新操作失败",
+  "-1903": "数据库删除操作失败"
+};
+
+var CDKEY = "0SDK-EBB-6688-JFSNK";
+var PASSWORD = "793178";
+
+SmSender = function () {
+
+  var ENDPOINT = "http://sdk4rptws.eucp.b2m.cn:8080/sdkproxy";
+
+  function parseResultContent(content) {
+    return xml2js.parseStringSync(content, {explicitRoot: false, explicitArray: false});
+  }
+
+  function activate() {
+    var resultContent = HTTP.post(ENDPOINT + "/regist.action", {
+      params: {
+        cdkey: CDKEY,
+        password: PASSWORD
+      }
+    }).content;
+
+    var resultData = parseResultContent(resultContent);
+
+    if (resultData.error !== '0')
+      throw new Error(resultData.message || ERR_MSGS[resultData.error]);
+  }
+
+  return {
+    init: _.once(activate),
+    batchSend: function (mobiles, message) {
+      check(mobiles, Match.OneOf(String, [String]));
+      check(message, String);
+
+      if (Match.test(mobiles, [String])) {
+        mobiles = mobiles.join();
+      }
+
+      var resultContent = HTTP.post(ENDPOINT + "/sendsms.action", {
+        params: {
+          cdkey: CDKEY,
+          password: PASSWORD,
+          phone: mobiles,
+          message: message
+        }
+      }).content;
+
+      var resultData = parseResultContent(resultContent);
+
+      if (resultData.error !== '0')
+        throw new Error(resultData.message || ERR_MSGS[resultData.error]);
+    }
+  };
+}();
+
+Meteor.defer(SmSender.init);
