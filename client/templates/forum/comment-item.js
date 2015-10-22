7053d88897b32c8ec305af381c5c0aaff42bc06a
简彦敏 <792074052@qq.com>
2015-10-22 23:39:09 +0800

    forum页面调整

--- client/templates/forum/comment-item.js
+++ client/templates/forum/comment-item.js
@@ -2,3 +2,15 @@
  * Created by jianyanmin on 15/10/6.
  */
 
+Template.commentItem.helpers({
+  myComment: function () {
+    console.log(this.userId);
+    if(this.userId==Meteor.userId()){
+      return false;
+    } else {
+      return true;
+    }
+  }
+});
+
+
